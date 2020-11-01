dd3f9154e726ee5cb6c70481684b9df4889b3151
mouse_484 <escmouse484@gmail.com>
2020-11-01 06:51:24 +0000

    ✨ : Implement command function

--- src/Client.ts
+++ src/Client.ts
@@ -1,18 +1,35 @@
 import {
   Client as DiscordClient,
-  //   ClientOptions as DiscordClientOptions,
+  ClientOptions as DiscordClientOptions,
+  Message,
 } from 'discord.js';
 
-// interface EcstarOptions extends DiscordClientOptions {
-//   prefix: string;
-// }
+interface EcstarOptions extends DiscordClientOptions {
+  prefix: string;
+}
 
 export class Client extends DiscordClient {
-  constructor() {
-    super();
+  constructor(options: EcstarOptions) {
+    super(options);
 
     super.once('ready', () => {
       console.log('ready');
+    });
+    super.on('message', async (message: Message) => {
+      if (!message.content.startsWith(options.prefix)) return;
+
+      const [commandName, ...args] = message.content
+        .slice(options.prefix.length)
+        .split(' ');
+
+      if (commandName === 'ping') {
+        const command = (await import('../examples/commands/ping')).default;
+        if (command) {
+          if (command?.render) {
+            command.render({ message, args });
+          }
+        }
+      }
     });
   }
 }
