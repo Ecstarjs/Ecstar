442a2e2c6fb5088d524a3bd99c3a153d8afe24de
mouse_484 <escmouse484@gmail.com>
2021-03-06 05:57:41 +0000

    context use parser

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,29 +1,23 @@
 import { Client } from 'ecstar';
 import { APIMessage, Message, StringResolvable } from 'discord.js';
-import split from 'split-string';
+import { parser } from "ecstar/parser"
 
 export interface Context {
-  name: string;
-  message: Message;
-  args: string[];
-  send(content: StringResolvable | APIMessage): Promise<Message>;
+    name: string;
+    message: Message;
+    args: string[];
+    send(content: StringResolvable | APIMessage): Promise<Message>;
 }
 
 export const context = (client: Client, message: Message): Context => {
-  const [prefixAndCommandName, ...args] = split(message.content, {
-    separator: ' ',
-    quotes: ['"', "'", '`'],
-    // brackets: { '<@': '>' },
-  });
+    const { commandName, args } = parser(client, message.cleanContent)
 
-  const commandName = prefixAndCommandName.slice(client.options.prefix.length);
-
-  return {
-    name: commandName,
-    message,
-    args,
-    send(content) {
-      return message.channel.send(content);
-    },
-  };
+    return {
+        name: commandName,
+        message,
+        args,
+        send(content) {
+            return message.channel.send(content);
+        },
+    };
 };
