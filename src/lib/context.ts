9df36110a4c8ad4b2f5cee9b81fdccfa23a1737a
mouse_484 <escmouse484@gmail.com>
2021-03-06 06:02:03 +0000

    fmt

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,23 +1,23 @@
 import { Client } from 'ecstar';
 import { APIMessage, Message, StringResolvable } from 'discord.js';
-import { parser } from "ecstar/parser"
+import { parser } from 'ecstar/parser';
 
 export interface Context {
-    name: string;
-    message: Message;
-    args: string[];
-    send(content: StringResolvable | APIMessage): Promise<Message>;
+  name: string;
+  message: Message;
+  args: string[];
+  send(content: StringResolvable | APIMessage): Promise<Message>;
 }
 
 export const context = (client: Client, message: Message): Context => {
-    const { commandName, args } = parser(client, message.cleanContent)
+  const { commandName, args } = parser(client, message.cleanContent);
 
-    return {
-        name: commandName,
-        message,
-        args,
-        send(content) {
-            return message.channel.send(content);
-        },
-    };
+  return {
+    name: commandName,
+    message,
+    args,
+    send(content) {
+      return message.channel.send(content);
+    },
+  };
 };
