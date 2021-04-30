0cd785d0145231ab666a964825ac4bda66ade148
mouse_484 <escmouse484@gmail.com>
2021-04-30 10:54:32 +0000

    event/command context add send func

--- src/lib/context/command.ts
+++ src/lib/context/command.ts
@@ -1,5 +1,11 @@
 import { Client } from 'ecstar';
-import { APIMessage, Message, StringResolvable } from 'discord.js';
+import {
+  APIMessage,
+  Message,
+  Snowflake,
+  StringResolvable,
+  TextChannel,
+} from 'discord.js';
 
 import { ContextBase } from 'ecstar/context';
 import { parser } from 'ecstar/parser';
@@ -9,7 +15,10 @@ export interface CommandContext extends ContextBase {
   type: 'command';
   message: Message;
   getArgs<T extends { [key: string]: keyof TypeList }>(types: T): argsType<T>;
-  send(content: StringResolvable | APIMessage): Promise<Message>;
+  send(
+    content: StringResolvable | APIMessage,
+    channelID?: Snowflake
+  ): Promise<Message>;
 }
 
 export const commandContext = (
@@ -24,8 +33,12 @@ export const commandContext = (
     client,
     message,
     getArgs: getArgs(args),
-    send(content) {
-      return message.channel.send(content);
+    send(content, id) {
+      const channel = id
+        ? (client.channels.cache.get(id) as TextChannel)
+        : message.channel;
+
+      return channel.send(content);
     },
   };
 };
