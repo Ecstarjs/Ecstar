72a0e7f12fca9a32e03a85d12df5a06a0d63296f
mouse_484 <escmouse484@gmail.com>
2021-04-30 10:54:32 +0000

    event/command context add send func

--- examples/events/message.ts
+++ examples/events/message.ts
@@ -1,14 +1,13 @@
-import { TextChannel } from 'discord.js';
 import { event } from 'ecstar';
 
 export default event(() => ({
   name: 'message',
-  run({ client }, [message]) {
+  run({ send }, [message]) {
     if (message.author.bot) return;
-    const channel = client.channels.cache.get(
-      '836949563704082482'
-    ) as TextChannel;
 
-    channel.send(`${message.cleanContent} | ${message.author.tag}`);
+    send(
+      `${message.cleanContent} | ${message.author.tag}`,
+      '836949563704082482'
+    );
   },
 }));
