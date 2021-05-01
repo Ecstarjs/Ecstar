08089ce467339e7f5e105e1f7e90b01875d088a2
mouse_484 <escmouse484@gmail.com>
2021-05-01 08:14:45 +0000

    Allow multiple events

--- examples/events/ready.ts
+++ examples/events/ready.ts
@@ -3,6 +3,6 @@ import { event } from 'ecstar';
 export default event(() => ({
   name: 'ready',
   run({ client }) {
-    process.stdout.write(`ready ${client.user?.tag}`);
+    client.log.ready(client.user?.tag);
   },
 }));
