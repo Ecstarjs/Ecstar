5252d4149accafd1faeb2fe47c81fa77d09857d9
mouse_484 <escmouse484@gmail.com>
2022-10-23 08:53:51 +0000

    feat: enhanced store log

--- src/lib/store/EventStore.ts
+++ src/lib/store/EventStore.ts
@@ -5,11 +5,11 @@ export class EventStore extends StoreBase<'event', Structures['event'][]> {
   constructor() {
     super('events');
   }
-  async update(path: string) {
+  async update(path: string, isCore?: boolean) {
     const { default: file }: { default: Structures['event'] } = await import(
       path
     );
     const current = this.get(file.name);
-    this.add(file.name, current ? [file, ...current] : [file]);
+    this.add(file.name, current ? [file, ...current] : [file], path, isCore);
   }
 }
