62979215b4f8effa87f26ee951e7adece2ecd98f
mouse_484 <escmouse484@gmail.com>
2022-10-23 13:13:17 +0000

    feat: [alpha] slash command

---
+++ src/lib/context/slashCommand.ts
@@ -0,0 +1,23 @@
+import { Interaction, Message, Snowflake, TextChannel } from 'discord.js';
+import { Client } from 'ecstar';
+import { ContextBase } from 'ecstar/context';
+
+export interface SlashCommandContext extends ContextBase {
+  type: 'slashCommand';
+  send(content: string): void;
+}
+
+export const slashCommandContext = (
+  client: Client,
+  interaction: Interaction
+): SlashCommandContext | undefined => {
+  if (!interaction.isChatInputCommand()) return;
+  return {
+    name: interaction.commandName,
+    type: 'slashCommand',
+    client,
+    send(content) {
+      interaction.reply(content);
+    },
+  };
+};
