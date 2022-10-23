9be5459689b7433b2bed083afa32cbe7ab94bdfd
mouse_484 <escmouse484@gmail.com>
2022-10-23 12:45:35 +0000

    feat: registering slash command

---
+++ src/core/events/ready/RegisteringSlashCommand.ts
@@ -0,0 +1,30 @@
+import { event } from 'ecstar';
+import { Events, REST, Routes, SlashCommandBuilder } from 'discord.js';
+
+export default event(() => ({
+  name: Events.ClientReady,
+  run({ client }) {
+    const commands: SlashCommandBuilder[] = [];
+
+    for (const key of client.commands.keys()) {
+      commands.push(
+        new SlashCommandBuilder()
+          .setName(key.toLowerCase())
+          .setDescription('ecstar slash command')
+      );
+    }
+
+    const rest = new REST({ version: '10' }).setToken(client?.token || '');
+
+    (async () => {
+      try {
+        await rest.put(Routes.applicationCommands(client.user?.id || ''), {
+          body: commands,
+        });
+        client.log.success('slash commands');
+      } catch (error) {
+        client.log.error(error);
+      }
+    })();
+  },
+}));
