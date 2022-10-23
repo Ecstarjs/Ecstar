b5b5f11e66163e1252af6b664bf1a1859fc9ec35
mouse_484 <escmouse484@gmail.com>
2022-10-23 08:53:51 +0000

    feat: enhanced store log

--- src/lib/store/StoreBase.ts
+++ src/lib/store/StoreBase.ts
@@ -14,24 +14,33 @@ export class StoreBase<
   T extends keyof Structures,
   X extends Structures[T] | Structures[T][] = Structures[T]
 > extends Map<string, X> {
-  constructor(private storeDirectryName: string) {
+  constructor(storeDirectryName: string) {
     super();
 
     const directorypath = getDirectoryPath(storeDirectryName);
     if (!directorypath) return;
-    watch([directorypath, path.resolve(__dirname, '../../core/')]).on(
+    watch(directorypath).on('add', (path) => this.update(path));
+
+    watch(path.resolve(__dirname, '../../core/', storeDirectryName)).on(
       'add',
-      (path) => this.update(path)
+      (path) => this.update(path, true)
     );
   }
-  add(name: string, file: X): this {
-    Client.log.info('[Store]', `/${this.storeDirectryName} - ${name}`);
+  public add(name: string, file: X, filePath: string, isCore?: boolean): this {
+    Client.log.info(
+      `[Store] `,
+      isCore ? '(core)' : '      ',
+      ' ',
+      name.padEnd(20, ' '),
+      '`' + filePath.split('/').slice(-2).join('/') + '`'
+    );
     return this.set(name, file);
   }
-  async update(path: string) {
+  async update(path: string, isCore?: boolean) {
     const { default: file }: { default: X } = await import(path);
+
     if ('name' in file) {
-      this.add(file.name, file);
+      this.add(file.name, file, path, isCore);
     }
   }
 }
