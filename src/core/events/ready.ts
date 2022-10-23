ed9c0478b7a5b1582c6fe92791474f8d9fbc929c
mouse_484 <escmouse484@gmail.com>
2022-10-23 08:20:49 +0000

    feat: enhanced ready log

--- src/core/events/ready.ts
+++ src/core/events/ready.ts
@@ -4,6 +4,9 @@ import { Events } from 'discord.js';
 export default event(() => ({
   name: Events.ClientReady,
   run({ client }) {
-    client.log.ready(client.user?.tag);
+    client.log.ready(
+      `account : \`${client.user?.tag}\``,
+      `|  prefix : \`${client.options.prefix}\``
+    );
   },
 }));
